5707dbfea2f407efc6ed629545b83f40a4545133
Luis Felipe <luisfelipe.araujo.contato@gmail.com>
2022-04-19 22:36:21 -0300

    creating the pages: about,help and home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get '/about', to: 'pages#about'
+  get '/help', to: 'pages#help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
