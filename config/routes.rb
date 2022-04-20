29bbd702e4f101c74512b22025c117894aace2cf
Luis Felipe <luisfelipe.araujo.contato@gmail.com>
2022-04-20 11:46:11 -0300

    creating controllers and routes for todos

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get '/about', to: 'pages#about'
   get '/help', to: 'pages#help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resources:todos
   
 end
