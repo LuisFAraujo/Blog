59bcd9c7d1640ef3881f40631358c758a242ad3e
Luis Felipe <luisfelipe.araujo.contato@gmail.com>
2022-04-20 21:18:44 -0300

    deleting todo complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,40 +1,57 @@
 class TodosController < ApplicationController
     
+    def index 
+        @todos = Todo.all
+    end
+
+    def create 
+        @todo = Todo.new(todo_params)
+             
+        respond_to do |format|  
+            if @todo.save
+                format.html {redirect_to todo_path(@todo), notice: "Todo was created successfully"}
+                
+            else
+                format.html { render :new, status: :unprocessable_entity}
+                  
+            end  
+        end
+    end
+
     def new
         @todo = Todo.new
     end
 
     def show
         @todo = Todo.find(params[:id])
-   
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        respond_to do |format|  
+            if @todo.update(todo_params)
+                format.html {redirect_to todo_path(@todo), notice: "Todo was updated successfully"}
+                
+            else
+                format.html { render :edit, status: :unprocessable_entity}
+                  
+            end  
+        end
+
     end
 
     def edit
         @todo = Todo.find(params[:id])
     end
-
-    def update
+    
+    def destroy 
         @todo = Todo.find(params[:id])
-        if @todo.update(todo_params)
-            flash[:notice] = "todo was succesfully updated"
-            redirect_to todo_path(@todo)
-        else
-            render 'edit'
-        end
-
+        @todo.destroy
+        flash[:notice] = "Todo was succesfully deleted"
+        redirect_to todos_path
     end
     
-    def create 
-        @todo = Todo.new(todo_params)       
-            if @todo.save
-                flash[:notice] = "Todo successfully created"
-                redirect_to todo_path(@todo)
-                
-            else
-                render "new"
-                  
-            end  
-    end
+    
     
     private
 
