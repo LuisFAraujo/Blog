1a8cf11c6c678075efcaad47ea7bb03a6579b249
Luis Felipe <luisfelipe.araujo.contato@gmail.com>
2022-04-20 21:44:04 -0300

    refactoring todo code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,16 @@
 class TodosController < ApplicationController
-    
+    before_action :set_todo, only: [:show, :update, :edit, :destroy]
+
     def index 
         @todos = Todo.all
     end
-
     def create 
-        @todo = Todo.new(todo_params)
-             
+        @todo = Todo.new(todo_params)            
         respond_to do |format|  
             if @todo.save
-                format.html {redirect_to todo_path(@todo), notice: "Todo was created successfully"}
-                
+                format.html {redirect_to todo_path(@todo), notice: "Todo was created successfully"}              
             else
-                format.html { render :new, status: :unprocessable_entity}
-                  
+                format.html { render :new, status: :unprocessable_entity}              
             end  
         end
     end
@@ -23,38 +20,36 @@ class TodosController < ApplicationController
     end
 
     def show
-        @todo = Todo.find(params[:id])
     end
 
     def update
-        @todo = Todo.find(params[:id])
+
         respond_to do |format|  
             if @todo.update(todo_params)
-                format.html {redirect_to todo_path(@todo), notice: "Todo was updated successfully"}
-                
+                format.html {redirect_to todo_path(@todo), notice: "Todo was updated successfully"}                
             else
-                format.html { render :edit, status: :unprocessable_entity}
-                  
+                format.html { render :edit, status: :unprocessable_entity}                
             end  
         end
 
     end
 
     def edit
-        @todo = Todo.find(params[:id])
     end
     
     def destroy 
-        @todo = Todo.find(params[:id])
         @todo.destroy
-        flash[:notice] = "Todo was succesfully deleted"
-        redirect_to todos_path
+        respond_to do |format|
+            format.html {redirect_to todos_path, notice: "Todo was deleted successfully", status: :see_other}
+        end
     end
     
     
     
     private
-
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
         def todo_params
             params.require(:todo).permit(:name, :description)
         end
